0617a5e6f30a6b92279b684654996da153b6fa5d
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-07-22 13:52:22 +0300

    added detect_and_parse method

--- lib/html_images_extractor.rb
+++ lib/html_images_extractor.rb
@@ -1,5 +1,16 @@
-require "html_images_extractor/version"
+require 'html_images_extractor/version'
+require 'html_images_extractor/html'
 
 module HtmlImagesExtractor
-  # Your code goes here...
+  def self.detect_and_parse(input_file_path)
+    raise 'Input file missing' if input_file_path.nil?
+
+    extension = File.extname(input_file_path)
+    case extension
+    when /\.html/i
+      then Html.parse(input_file_path)
+    else
+      raise 'File extension not html'
+    end
+  end
 end
